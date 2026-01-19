300f585f958d22dee3d40a658181a4f72e40edbe
Guilherme Amaral <guilherme.amaral9@hotmail.com>
2026-01-19 20:25:39 -0300

    fix: deploy no gh-pages atualizado

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { PokemonDetails } from './pages/PokemonDetails';
 import { Header } from './components/Header';
 
 export const AppRoutes = () => {
   return (
-    <BrowserRouter basename='/pokedex'>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pokemon/:id" element={<PokemonDetails />} />
       </Routes>
-    </BrowserRouter>
+      </HashRouter>
   );
 };
